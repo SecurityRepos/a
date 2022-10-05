69721a2859f4894ba60fe59688deafac028c26a5
Hüsü <73742151+husudu@users.noreply.github.com>
2022-10-05 22:39:44 +0300

    Fix

--- userbot/modules/__helpme.py
+++ userbot/modules/__helpme.py
@@ -1,16 +1,14 @@
 # Copyright Brend Userbot
 # Bu modul Brend Userbota Məxsusdur
+
 from userbot import BOT_USERNAME
 from userbot.events import register
 from userbot.language import get_value
 LANG = get_value("__helpme")
 
-@register(outgoing=True, pattern="^.komek|^.help")
-async def komek(event):
+@register(outgoing=True, pattern="^.help$")
+async def help(event):
     brend = BOT_USERNAME
-    if brend is not None:
-        results = await event.client.inline_query(brend, "@BrendUserbot")
-        await results[0].click(event.chat_id, reply_to=event.reply_to_msg_id, hide_via=True)
-        await event.delete()
-    else:
-        await event.edit(LANG["NO_BOT"])
+    results = await event.client.inline_query(brend, "@BrendUserbot")
+    await results[0].click(event.chat_id, reply_to=event.reply_to_msg_id, hide_via=True)
+    await event.delete()
