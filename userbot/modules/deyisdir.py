b0509070d2064205c41049592c60954770df5014
Hüsü <73742151+husudu@users.noreply.github.com>
2022-10-08 23:19:07 +0300

    Buda belə

--- userbot/modules/deyisdir.py
+++ userbot/modules/deyisdir.py
@@ -11,6 +11,7 @@ LANG = get_value("degistir")
 @register(outgoing=True, pattern="^.d[eə]yi[sş]dir ?(.*)")
 async def deyisdir(event):
     plugin = event.pattern_match.group(1)
+    yenimsj = event.pattern_match.group(2)
     mesaj = re.search(r"\"(.*)\"", plugin)
     if mesaj:
         rege = re.findall(r"(?:|$)(.*)\"(.*)\"", plugin)
@@ -19,7 +20,7 @@ async def deyisdir(event):
     else:
         mesaj = []
     plugin = plugin.strip()
-    NOVLER = ["afk", "alive", "alives", "pm", "kickme", "dızcı", "ban", "mute", "approve", "tagsleep", "disapprove", "block"]
+    NOVLER = ["afk", "unafk", "alive", "alives", "pm", "kickme", "dızcı", "ban", "mute", "approve", "tagsleep", "disapprove", "block"]
     if type(mesaj) == list:
         if plugin in NOVLER:
             if event.is_reply:
@@ -31,7 +32,11 @@ async def deyisdir(event):
                     return await event.edit(f"🆕 `{plugin}` {LANG['SETTED_MEDIA']}")
                 PLUGIN_MESAJLAR[plugin] = reply.text
                 sql.ekle_mesaj(plugin, reply.text)
-                return await event.edit(f"🆕 `{plugin}` {LANG['SETTED_REPLY']}")   
+                return await event.edit(f"🆕 {plugin} {LANG['SETTED_REPLY']} {reply.text}")   
+            if yenimsj:
+                PLUGIN_MESAJLAR[plugin] = yenimsj
+                sql.ekle_mesaj(plugin, yenimsj)
+                return await event.edit(f"🆕 {plugin} {LANG['SETTED_REPLY'] {yenimsj}}")
 
             silme = sql.sil_mesaj(plugin)
             if silme == True:
@@ -40,7 +45,7 @@ async def deyisdir(event):
             else:
                 await event.edit(f"{LANG['ERROR_DELETED']}: `{silme}`")
         else:
-            await event.edit(LANG['NOT_FOUND'] + ":`afk/alive/alives/pm/kickme/dızcı/ban/mute/approve/tagsleep/disapprove/block`")
+            await event.edit(LANG['NOT_FOUND'] + ":`afk/unafk/alive/alives/pm/kickme/dızcı/ban/mute/approve/tagsleep/disapprove/block`")
     elif len(plugin) < 1:
         await event.edit(LANG['USAGE'])
     elif type(mesaj) == str:
@@ -53,13 +58,14 @@ async def deyisdir(event):
                 sql.ekle_mesaj(plugin, mesaj)
                 await event.edit(LANG['SETTED'].format(plu=plugin, msj=mesaj))
         else:
-            await event.edit(LANG['NOT_FOUND'] + ":`afk/alive/alives/pm/kickme/dızcı/ban/mute/approve/tagsleep/disapprove/block`")
+            await event.edit(LANG['NOT_FOUND'] + ":`afk/unafk/alive/alives/pm/kickme/dızcı/ban/mute/approve/tagsleep/disapprove/block`")
 
 CmdHelp('change').add_command('change və ya d[eə]yi[sş]dir', '<modul> <mesaj və ya cavab>', 'Dəyişdir əmri, botdakı plugin mesajlarını dəyişdirmənizə yarayır. Əgər mesaj yazmasanız Plugin mesajını orijinal vəziyyətinə qaytarar.').add_info(
     '**Dəyişəbilən Pluginlər:** `afk/alive/alives/pm/kickme/dızcı/ban/mute/approve/tagsleep/disapprove/block`\n\
 **Alive Dəyişənləri:** `{plugin}, {telethon}, {brend}, {python}`\n\
 **Ban/Mute Dəyişənləri:** `{id}, {username}, {first_name}, {last_name}, {mention}, {date}, {count}`\n\
 **AFK Dəyişənləri:** `{username}, {mention}, {first_name}, {last_name}, {last_seen_seconds}, {last_seen}, {last_seen_long}`\n\
+**UNAFK Dəyişənləri:** `{time}, {username}, {mention}, {first_name}, {last_name}`\n\
 **PMpermit Dəyişkənləri(pm, block, approve, disapprove):** `{id}, {username}, {mention}, {first_name}, {last_name}`\n\
 **Kickme Dəyişəni:** `{istədiyiniz mətn}`'
 ).add()
